a8258e62310fe71125ab4312f7d29c32f1698cdc
Lorenzo Hess <lorenzohess@tutanota.com>
2026-05-03 19:48:31 -0400

    almost working adaptive traj sine

--- project_code/adaptive_on_arm.m
+++ project_code/adaptive_on_arm.m
@@ -46,18 +46,18 @@ R_inv = pinv(R_gain);
 alpha_acc = 0.2;                 % low-pass on finite-diff qddot
 
 %% Constant trajectory
-Kp = diag([0.15 0.15 0.5 0.25]);
-Kv = diag([0.1 0.08 0.075 0.05]);
-t_sample       = 0.04;
-tfin           = 15;
-t = 0:t_sample:tfin;
-q1_desired = deg2rad(39)*ones(1, length(t));
-q2_desired = deg2rad(-28)*ones(1, length(t));
-q3_desired = deg2rad(55)*ones(1, length(t));
-q4_desired = deg2rad(-57)*ones(1, length(t));
-q_desired = [q1_desired; q2_desired; q3_desired; q4_desired];
-q_desired_dot = [0*q1_desired; 0*q2_desired; 0*q3_desired; 0*q4_desired];
-q_desired_ddot = [0*q1_desired; 0*q2_desired; 0*q3_desired; 0*q4_desired];
+% Kp = diag([0.15 0.15 0.5 0.25]);
+% Kv = diag([0.1 0.08 0.075 0.05]);
+% t_sample       = 0.04;
+% tfin           = 15;
+% t = 0:t_sample:tfin;
+% q1_desired = deg2rad(39)*ones(1, length(t));
+% q2_desired = deg2rad(-28)*ones(1, length(t));
+% q3_desired = deg2rad(55)*ones(1, length(t));
+% q4_desired = deg2rad(-57)*ones(1, length(t));
+% q_desired = [q1_desired; q2_desired; q3_desired; q4_desired];
+% q_desired_dot = [0*q1_desired; 0*q2_desired; 0*q3_desired; 0*q4_desired];
+% q_desired_ddot = [0*q1_desired; 0*q2_desired; 0*q3_desired; 0*q4_desired];
 
 
 %% Load desired trajectory from file 
@@ -71,6 +71,38 @@ q_desired_ddot = [0*q1_desired; 0*q2_desired; 0*q3_desired; 0*q4_desired];
 % t = 0:t_sample:tfin;
 % Kp = diag([0.2 0.3 0.3 0.2]);
 % Kv = diag([0.05 0.05 0.05 0.05]);
+
+%% Sinusoidal
+Kp = diag([0.2 0.35 0.6 0.2]);
+Kv = diag([0.075 0.05 0.05 0.05]);
+t_sample = 0.04;
+tfin = 10;
+t = 0:t_sample:tfin;
+
+omega = 1.0;  % angular frequency (rad/s). Feel free to change (e.g. 0.5 for slower motion, 2.0 for faster)
+
+q1_desired = 0.5 * sin(omega * t);
+q2_desired = -0.5 * sin(omega * t);
+q3_desired = -0.5 * sin(omega * t);          % sinusoidal position \pm 0.5 rad
+q4_desired = -0.5 * sin(omega * t);
+
+q_desired = [q1_desired; q2_desired; q3_desired; q4_desired];
+
+% Velocity (automatically sinusoidal)
+q1_desired_dot = 0.5 * omega * cos(omega * t);
+q2_desired_dot = -0.5 * omega * cos(omega * t);
+q3_desired_dot = -0.5 * omega * cos(omega * t);   % velocity = d(q3)/dt
+q4_desired_dot = -0.5 * omega * cos(omega * t);
+
+q_desired_dot = [q1_desired_dot; q2_desired_dot; q3_desired_dot; q4_desired_dot];
+
+% Acceleration (automatically sinusoidal)
+q1_desired_ddot = -0.5 * omega^2 * sin(omega * t);
+q2_desired_ddot = 0.5 * omega^2 * sin(omega * t);
+q3_desired_ddot = 0.5 * omega^2 * sin(omega * t);  % acceleration = d²(q3)/dt²
+q4_desired_ddot = 0.5 * omega^2 * sin(omega * t);
+
+q_desired_ddot = [q1_desired_ddot; q2_desired_ddot; q3_desired_ddot; q4_desired_ddot];
 
 Mbar = M_fun(q_desired(:,1), p_bar);
 A = [zeros(n) eye(n); -pinv(Mbar)*Kp -pinv(Mbar)*Kv];
